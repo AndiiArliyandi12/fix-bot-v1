69ec51a04f37ef09347d908132fc344393afd27a
diskahaha <109210020+diskahaha@users.noreply.github.com>
2022-09-10 12:12:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -119,11 +119,11 @@ global.author = '                「 𝕭𝖔𝖙 • [A]ndii ∅༢࿔ྀ」'
 
 
 /*============== LOGO ==============*/
-global.thumb = 'https://i.ibb.co/XZrK6yQ/transformers.jpg' //Main Thumbnail
-global.imagebot = 'https://i.ibb.co/XZrK6yQ/transformers.jpg'
-global.giflogo = 'https://telegra.ph/file/a46ab7fa39338b1f54d5a.mp4'
-global.thumbs = ['https://i.ibb.co/XZrK6yQ/transformers.jpg']
-global.fla = 'https://i.ibb.co/XZrK6yQ/transformers.jpg'
+global.thumb = 'https://telegra.ph/file/c73f47b440241b66cb9d3.jpg' //Main Thumbnail
+global.imagebot = 'https://telegra.ph/file/805a37b1e9a963e7d7ecf.jpg'
+global.giflogo = 'https://telegra.ph/file/c73f47b440241b66cb9d3.jpg'
+global.thumbs = ['https://telegra.ph/file/c73f47b440241b66cb9d3.jpg']
+global.fla = 'https://telegra.ph/file/c73f47b440241b66cb9d3.jpg'
 global.flaaa2 = [
 'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&script=water-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextColor=%23000&shadowGlowColor=%23000&backgroundColor=%23000&text=',
 'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=crafts-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&text=',
