b2079a1484e4a97d2c269738389b5f925fd069a4
diskahaha <109210020+diskahaha@users.noreply.github.com>
2022-09-11 15:00:43 +0700

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -6,9 +6,9 @@ let str = `Nyari Apa Mek? 💦`
 let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://Instagram.com/f",
+    mediaUrl: "https://chat.whatsapp.com/EjczE2AmL7qHnpPbs8d74b",
     mediaType: "VIDEO",
-    description: "https://youtu.be/zc7_OTzW4dM", 
+    description: "https://chat.whatsapp.com/EjczE2AmL7qHnpPbs8d74b", 
     title: '𝕭𝖔𝖙 ∅༢࿔ྀ',
     body: wm,
     thumbnail: thumb,
