3733a5afdbaa55e835bd67475675d1b920c76f23
diskahaha <109210020+diskahaha@users.noreply.github.com>
2022-09-12 17:43:14 +0700

    Update youtube-yts.js

--- plugins/youtube-yts.js
+++ plugins/youtube-yts.js
@@ -1,33 +1,32 @@
 import { youtubeSearch } from '@bochilteam/scraper'
-
-let handler = async(m, { conn, usedPrefix, text, args, command }) => {
-let fdoc = {quoted:{key : {participant : '0@s.whatsapp.net'},message: {documentMessage: {title: `${command}`}}}}
-let imgr = flaaa.getRandom()
-
+let handler = async (m, { text }) => {
   if (!text) throw 'Cari apa?'
   const { video, channel } = await youtubeSearch(text)
   let teks = [...video, ...channel].map(v => {
     switch (v.type) {
       case 'video': return `
-📌 *${v.title}* (${v.url})
-⌚ Duration: ${v.durationH}
-⏲️ Uploaded ${v.publishedTime}
+💌 *${v.title}* 
+🔗 _${v.url}_
+⏰ Duration: ${v.durationH}
+📤 Uploaded ${v.publishedTime}
 👁️ ${v.view} views
       `.trim()
       case 'channel': return `
-📌 *${v.channelName}* (${v.url})
-🧑‍🤝‍🧑 _${v.subscriberH} (${v.subscriber}) Subscriber_
-🎥 ${v.videoCount} video
+╭──────━• *CHANNEL*
+│🎀 *${v.channelName}* 
+│🔗 _${v.url}_
+│📛 _${v.subscriberH} Subscriber_
+│🎥 ${v.videoCount} video
+┗──────━•
 `.trim()
     }
-  }).filter(v => v).join('\n\n▣═━–〈 *YT SEARCH* 〉–━═▣\n\n')
-  //m.reply(teks)
-  await conn.sendButton(m.chat, teks, wm, `${imgr + command}`, [
-                ['Youtube Search🔎', `${usedPrefix + command} ${text}`]
-            ], m, fdoc)
+  }).filter(v => v).join('\n\n─────────────━─────────────\n\n')
+  m.reply(`*${htki} SEARCH ${htka}*\n\n` + teks)
 }
 handler.help = ['', 'earch'].map(v => 'yts' + v + ' <pencarian>')
 handler.tags = ['tools']
 handler.command = /^yts(earch)?$/i
+
+handler.register = true
 
 export default handler
