4f13928aafd7b00248896f6699d4ba21f7126432
diskahaha <109210020+diskahaha@users.noreply.github.com>
2022-09-29 08:54:39 +0700

    Update ppcouple.js

--- plugins/ppcouple.js
+++ plugins/ppcouple.js
@@ -1,16 +1,32 @@
-/* import fetch from 'node-fetch'
+/*import fetch from 'node-fetch'
+let handler = async (m, { conn, command }) => {
+let res = await fetch(`https://api.lolhuman.xyz/api/random/ppcouple?apikey=${lolkey}`)
+if (res.status != 200) throw await res.text()
+let json = await res.json()
+if (!json.status) throw json
+conn.sendButton(m.chat, 'Cewek', wm, json.result.female,[['NEXT', `/${command}`]], m)
+conn.sendButton(m.chat, 'Cowok', wm, json.result.male, [['NEXT', `/${command}`]], m)
+}
+handler.help = ['ppcouple']
+handler.tags = ['internet']
+handler.command = /^(ppcp|ppcouple)$/i
+export default handler*/
 
+
+import fetch from "node-fetch"
 let handler = async (m, { conn }) => {
-  let res = await fetch(global.API('LeysCoder', '/api/ppcouple', {}, 'apikey'))
-  if (!res.ok) throw await res.text()
-  let json = await res.json()
-  if (!json.status) throw json
-  await conn.sendFile(m.chat, json.result.male, '', 'Nih PP Yg Cowo', m)
-  await conn.sendFile(m.chat, json.result.female, '', 'Dan Ni PP YgCewe', m)
+
+  let data = await (await fetch('https://raw.githubusercontent.com/KazukoGans/database/main/anime/ppcouple.json')).json()
+  let cita = data[Math.floor(Math.random() * data.length)]
+  
+  let cowi = await(await fetch(cita.cowo)).buffer()
+  await conn.sendFile(m.chat, cowi, '', '♂️', m)
+  let ciwi = await(await fetch(cita.cewe)).buffer()
+  await conn.sendFile(m.chat, ciwi, '', '♀️', m)
 }
 handler.help = ['ppcouple', 'ppcp']
 handler.tags = ['internet']
 handler.command = /^(pp(cp|couple))$/i
 handler.limit = true
 
-export default handler */
+export default handler 
