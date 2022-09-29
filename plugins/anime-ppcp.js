be2849a77c42e24529d29f28fddfcd25bba836c0
diskahaha <109210020+diskahaha@users.noreply.github.com>
2022-09-29 08:58:43 +0700

    Update anime-ppcp.js

--- plugins/anime-ppcp.js
+++ plugins/anime-ppcp.js
@@ -1,17 +1,32 @@
-import fetch from 'node-fetch'
+/*import fetch from 'node-fetch'
 let handler = async (m, { conn, command }) => {
-/*let res = await fetch(`https://api.lolhuman.xyz/api/random/ppcouple?apikey=${lolkey}`)
+let res = await fetch(`https://api.lolhuman.xyz/api/random/ppcouple?apikey=${lolkey}`)
 if (res.status != 200) throw await res.text()
 let json = await res.json()
 if (!json.status) throw json
-conn.sendButton(m.chat, '𝙶𝚒𝚛𝚕𝚜', wm, json.result.female,[['🔄 Next 🔄', `/${command}`]], m)
-conn.sendButton(m.chat, '𝙱𝚘𝚢𝚜', wm, json.result.male, [['🔄 Next 🔄', `/${command}`]], m) */
-let jsn = await fetch(`https://ziy.herokuapp.com/api/gacha/ppcouple?apikey=xZiyy`)
-let json = await jsn.json()
-conn.sendButton(m.chat, '𝙶𝚒𝚛𝚕𝚜', wm, json.result.female,[['🔄 Next 🔄', `/${command}`]], m)
-conn.sendButton(m.chat, '𝙱𝚘𝚢𝚜', wm, json.result.male, [['🔄 Next 🔄', `/${command}`]], m)
+conn.sendButton(m.chat, 'Cewek', wm, json.result.female,[['NEXT', `/${command}`]], m)
+conn.sendButton(m.chat, 'Cowok', wm, json.result.male, [['NEXT', `/${command}`]], m)
 }
 handler.help = ['ppcouple']
 handler.tags = ['internet']
 handler.command = /^(ppcp|ppcouple)$/i
-export default handler
+export default handler*/
+
+
+import fetch from "node-fetch"
+let handler = async (m, { conn }) => {
+
+  let data = await (await fetch('https://raw.githubusercontent.com/KazukoGans/database/main/anime/ppcouple.json')).json()
+  let cita = data[Math.floor(Math.random() * data.length)]
+  
+  let cowi = await(await fetch(cita.cowo)).buffer()
+  await conn.sendFile(m.chat, cowi, '', '♂️', m)
+  let ciwi = await(await fetch(cita.cewe)).buffer()
+  await conn.sendFile(m.chat, ciwi, '', '♀️', m)
+}
+handler.help = ['ppcouple', 'ppcp']
+handler.tags = ['internet']
+handler.command = /^(pp(cp|couple))$/i
+handler.limit = true
+
+export default handler 
